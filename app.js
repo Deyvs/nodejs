b991df5d8a7615871da2637082223ba60b1c3136
Deyvison Silva <deyvisondasilva@gmail.com>
2023-02-01 13:13:29 -0300

    Working with dynamic content and Adding template engine - pug

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const PORT = 3000;
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404)
-        .sendFile(path.join(__dirname, 'views', '404.html'));
+        .render('404', {docTitle: 'Page Not Found'})
 });
 
-app.listen(3000);
-// console.log("Nodemon it's working!");
+app.listen(PORT);
