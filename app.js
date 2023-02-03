72d54bd444df2c01c4beb6e4b6f347359de93c86
Deyvison Silva <deyvisondasilva@gmail.com>
2023-02-03 12:06:39 -0300

    Converting the project to ejs with ejs tempalte engine and adding the includes to ejs files. Make a diretory for others templates engine files

--- app.js
+++ app.js
@@ -2,17 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 const PORT = 3000;
 
 const app = express();
 
-app.engine('hbs', expressHbs.engine({ 
-    defaultLayout: 'main-layout', 
-    extname:'hbs'
-}));
-
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -27,7 +21,9 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404)
-        .render('404', {pageTitle: 'Page Not Found'})
+        .render('404', {
+            pageTitle: 'Page Not Found',
+            path: ''})
 });
 
 app.listen(PORT, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -10,10 +10,7 @@ const products = [];
 router.get('/add-product', (req, res, next) => {
     res.render('add-product', { 
         pageTitle: 'Add Product', 
-        path: '/admin/add-product',
-        activeAddProduct: true,
-        productCSS: true,
-        formCSS: true
+        path: '/admin/add-product'
     });
 });
 
